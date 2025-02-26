75a2edb43cb99053144c6ceeca097288abc90c2f
Jun <junanthony.business@gmail.com>
2025-02-26 14:49:03 +1300

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Children } from "react";
 import HomePage from "./pages/HomePage";
 import Header from "./pages/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./pages/Footer";
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
           
         </Routes>
       </div>
+      <Footer />
     </Router>
   )
 }
