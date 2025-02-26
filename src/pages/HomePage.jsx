cd8fdace3824cd116cb370ec302c8f06f42c931e
Jun <junanthony.business@gmail.com>
2025-02-26 14:30:26 +1300

    Add logo in choose us

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "./HomeComponents/Hero";
 import Services from "./HomeComponents/Services";
+import ChooseUs from "./HomeComponents/ChooseUs";
 
 
 export default function HomePage() {
@@ -9,6 +10,7 @@ export default function HomePage() {
         
     <Hero />
     <Services />
+    <ChooseUs />
     </div>
   );
 }
